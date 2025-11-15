4f1fd1ffd177b1e8c9003fc8aba04b14bd3e9cd8
Egorick1845 <proegorweb@gmail.com>
2025-11-16 01:35:27 +0300

    Дорожная карта

--- Content.Client/UserInterface/Systems/EscapeMenu/EscapeUIController.cs
+++ Content.Client/UserInterface/Systems/EscapeMenu/EscapeUIController.cs
@@ -138,6 +138,14 @@ public sealed class EscapeUIController : UIController, IOnStateEntered<GameplayS
         };
         CorvaxGoob-Coins-end */
 
+        // ADT ROADMAP TWEAK
+        _escapeWindow.RoadmapButton.OnPressed += _ =>
+        {
+            CloseEscapeWindow();
+            UIManager.GetUIController<RoadmapUIController>().ToggleRoadmap();
+        };
+        // ADT ROADMAP TWEAK
+
         _escapeWindow.RulesButton.OnPressed += _ =>
         {
             CloseEscapeWindow();
